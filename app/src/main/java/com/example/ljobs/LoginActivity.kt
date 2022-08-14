6da1e24f28e1a03051a78719b80bd73eed19916c
StevenJosh <64672935+arecus10155@users.noreply.github.com>
2022-08-14 12:56:12 +0800

    14-08-2022
    Login
    Register
    Splash
    Testing

--- app/src/main/java/com/example/ljobs/LoginActivity.kt
+++ app/src/main/java/com/example/ljobs/LoginActivity.kt
@@ -15,7 +15,7 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding?.root)
 
         setUpActionBar()
-//TEddddddddddddddddddddddddddd
+//TEddddddddddddddddddddddddddddddaaa
 
     }
 
