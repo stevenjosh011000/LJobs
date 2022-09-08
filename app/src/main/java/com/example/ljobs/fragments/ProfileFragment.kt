d60c7313a23fd19bdd02eb7793c454b295fd4f12
StevenJosh <64672935+arecus10155@users.noreply.github.com>
2022-09-08 18:48:19 +0800

    08-09-2022
    Extra Layout

--- app/src/main/java/com/example/ljobs/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/ljobs/fragments/ProfileFragment.kt
@@ -1,17 +1,21 @@
 package com.example.ljobs.fragments
 
 
+import android.app.Dialog
 import android.graphics.Typeface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import com.example.ljobs.R
 import com.example.ljobs.Session.LoginPref
 import com.example.ljobs.UserApp
+import com.example.ljobs.UserDao
+import com.example.ljobs.databinding.DialogProfileUpdateBinding
 import com.example.ljobs.databinding.FragmentProfileBinding
 import kotlinx.coroutines.launch
 
@@ -35,8 +39,9 @@ class ProfileFragment : Fragment() {
 
         val typeFace: Typeface = Typeface.createFromAsset(activity?.applicationContext?.assets,"goodTimesRg.otf")
         val userDao = (activity?.applicationContext as UserApp).db.userDao()
-
+        val email: String = binding.tvProfileEmail.toString()
         var user:HashMap<String,String> = session.getUserDetails()
+
 
 
         binding.tvProfilePic.setOnClickListener {
@@ -55,8 +60,52 @@ class ProfileFragment : Fragment() {
                 }
             }
         }
+
+        binding.ivEdit.setOnClickListener {
+            updateProfileDialog(email,userDao)
+        }
+
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    private fun updateProfileDialog(email:String,userDao: UserDao){
+        val fragment : ProfileFragment = (activity?.supportFragmentManager?.findFragmentByTag("ProfileFragment") as ProfileFragment)
+        val updateDialog= Dialog(fragment.requireContext(), R.style.Theme_Dialog)
+        updateDialog.setCancelable(false)
+
+        val binding = DialogProfileUpdateBinding.inflate(layoutInflater)
+        updateDialog.setContentView(binding.root)
+
+//        lifecycleScope.launch{
+//            employeeDao.fetchEmployeeById(id).collect{
+//                if(it != null){
+//                    binding.etUpdateName.setText(it.name)
+//                    binding.etUpdateEmailId.setText(it.email)
+//                }
+//            }
+//        }
+//
+//        binding.tvUpdate.setOnClickListener {
+//            val name = binding.etUpdateName.text.toString()
+//            val email = binding.etUpdateEmailId.text.toString()
+//            if(name.isNotEmpty() && email.isNotEmpty()){
+//                lifecycleScope.launch{
+//                    employeeDao.update(EmployeeEntity(id,name,email))
+//                    Toast.makeText(applicationContext,"Record Updated.", Toast.LENGTH_LONG).show()
+//                    updateDialog.dismiss()
+//                }
+//            }else{
+//                Toast.makeText(applicationContext,"Name or Email cannot be blank.", Toast.LENGTH_LONG).show()
+//            }
+//        }
+
+        binding.tvCancel.setOnClickListener {
+            updateDialog.dismiss()
+        }
+
+        updateDialog.show()
+
     }
 
 
